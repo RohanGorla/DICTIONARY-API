7e7ffc5a352120eccf8f45129a1f59a4faa5424a
RohanGorla <rohangorla2010@gmail.com>
2024-02-20 08:12:11 +0530

    update html and css

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 function ron() {
+    document.querySelector("p").innerText = "";
     let wor = document.getElementById("word").value;    
     console.log(wor);
     let reqUrl = `${url}${wor}`;
 
     let p = fetch(reqUrl).
-    then((res)=>res.json()).
+    then((res)=>{
+        return res.json();
+    }).
     then((resp)=>{
+            console.log(resp);
             if (resp.title) {
                 document.querySelector("p").innerText = "Word doesn't exist in DATABASE!"
             } else {
